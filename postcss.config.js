11bf420f2705b89afba1e8f21e36282e37915064
James Reed <james@twiddlingbits.net>
2022-06-20 10:41:58 -0600

    Remove postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-const path = require("path");
-
-module.exports = () => ({
+module.exports = {
   plugins: [
-    require("tailwindcss")(path.resolve(__dirname, "tailwind.config.js")),
+    require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
-    process.env.FLASK_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [path.resolve(__dirname, "app/templates/*.html")],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
   ],
-});
+};
